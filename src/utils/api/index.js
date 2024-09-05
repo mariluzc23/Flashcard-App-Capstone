3bac02afbd3a25475850e71c72814856b5620eff
Mariluz <mariluz.colon23@gmail.com>
2024-09-05 11:57:42 -0400

    updating server

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,8 +2,7 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:3000/api';
-
+const API_BASE_URL = 'https://flashcard-json-server-38pw.onrender.com';
 
  /**
   * Defines the default headers for these functions to work with `json-server`
